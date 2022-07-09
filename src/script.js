37dd80ce0b2335dd87d315f29e460139d1184fbd
Cícero Borges <ciceropborges@gmail.com>
2022-07-09 17:49:20 -0300

    Adding Interactivity with State

--- src/script.js
+++ src/script.js
@@ -10,7 +10,13 @@
 
  // homepage component
  function HomePage() {
+    const [likes, setLikes] = React.useState(0);
+
     const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    function handleClick() {
+        setLikes(likes + 1)
+    }
 
     return (
         <>
@@ -20,6 +26,8 @@
                     <li key={name}>{name}</li>
                 ))}
             </ul>
+
+            <button onClick={handleClick}>Like ({likes})</button>
         </>
     )
  }
