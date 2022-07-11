0e4835a2fe9f7973cf05c8cac3724ade958e1f77
Cícero Borges <ciceropborges@gmail.com>
2022-07-11 08:18:12 -0300

    Example of useState

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,28 @@
+//import { useState } from 'react';
 import Link from 'next/link';
+
+// function PostButton(props) {
+//   const { name } = props;
+
+//   const [likes, setLikes] = useState(0);
+//   const [people, setPeople] = useState([
+//     {id: 1, name: 'Joao', idade: 27}, 
+//     {id:2, name: 'Cícero', idade: 23}
+//   ]);
+
+//   return (
+//     <>
+//       <ul>
+//         {people.map(person => (
+//           <li key={person.id}>{person.name} ({person.idade})</li>
+//         ))}
+//       </ul>
+//       <button onClick={() => setLikes(likes + 1)}>
+//         {`${name} (${likes})`}
+//       </button>
+//     </>
+//   )
+// }
 
 export default function FirstPost() {
   return (
@@ -9,6 +33,7 @@ export default function FirstPost() {
           <a>Back to home</a>
         </Link>
       </h2>
+      {/* <PostButton name="Clicadas"/> */}
     </>
   );
 }
