8d418bf18881093096880d493312b73f452df8a8
Cícero Borges <ciceropborges@gmail.com>
2022-07-11 23:46:59 -0300

    Assets, Metadata, and CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,39 +1,63 @@
-//import { useState } from 'react';
+import { useState } from 'react';
+
+import Head from 'next/head';
+import Script from 'next/script';
 import Link from 'next/link';
+import Image from 'next/image';
 
-// function PostButton(props) {
-//   const { name } = props;
+import Layout from '../../components/layout';
 
-//   const [likes, setLikes] = useState(0);
-//   const [people, setPeople] = useState([
-//     {id: 1, name: 'Joao', idade: 27}, 
-//     {id:2, name: 'Cícero', idade: 23}
-//   ]);
+function PostButton(props) {
+  const { name } = props;
 
-//   return (
-//     <>
-//       <ul>
-//         {people.map(person => (
-//           <li key={person.id}>{person.name} ({person.idade})</li>
-//         ))}
-//       </ul>
-//       <button onClick={() => setLikes(likes + 1)}>
-//         {`${name} (${likes})`}
-//       </button>
-//     </>
-//   )
-// }
+  const [likes, setLikes] = useState(0);
+  const [people, setPeople] = useState([
+    {id: 1, name: 'Joao', idade: 27}, 
+    {id:2, name: 'Cícero', idade: 23}
+  ]);
+
+  return (
+    <>
+      <ul>
+        {people.map(person => (
+          <li key={person.id}>{person.name} ({person.idade})</li>
+        ))}
+      </ul>
+      <button onClick={() => setLikes(likes + 1)}>
+        {`${name} (${likes})`}
+      </button>
+    </>
+  )
+}
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-      </h2>
-      {/* <PostButton name="Clicadas"/> */}
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+              console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        />
+        <h1>First Post</h1>
+        <Image
+          src="/images/profile.jpeg" // Route of the image file
+          height={144} // Desired size with correct aspect ratio
+          width={144} // Desired size with correct aspect ratio
+          alt="Your Name"
+        />
+        <h2>
+          <Link href="/">
+            <a>Back to home</a>
+          </Link>
+        </h2>
+        <PostButton name="Clicadas"/>
+      </Layout>
     </>
   );
 }
